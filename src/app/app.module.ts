5ee9e7ccaacc9469da0012c3a0b79974ef9d12bf
fkduah <fkduah23@gmail.com>
2016-11-13 18:38:18 -0500

    create routing

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
+
+
+// import { routing } from './app.routes';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { SearchComponent } from './components/search/search.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    NavbarComponent,
+    AboutComponent,
+    SearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule, 
+     RouterModule.forRoot([
+       {path: '', component: SearchComponent},
+       {path: 'about', component: AboutComponent}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+// import { RouterModule }   from '@angular/router';
+
+
+// export const routing =  RouterModule.forRoot([
+//     {path: 'search', component: SearchComponent},
+//     {path: 'about', component: AboutComponent}
+// ]);
